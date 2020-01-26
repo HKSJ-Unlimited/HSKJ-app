e09582ead83ac9016752ae6d6951ad2fc423b76e
rocknegi <rocknegi53@gmail.com>
2020-01-26 14:09:46 +0530

    change to GO index

--- components/VideosLayout.js
+++ components/VideosLayout.js
@@ -3,58 +3,70 @@ import { Text, StyleSheet, FlatList, Modal, Dimensions } from 'react-native'
 import { Container, Content, List, ListItem, Button } from 'native-base'
 import { View, Linking } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { BASE_URL } from 'react-native-dotenv'
 
 import TopHeader from './Header';
 import { get } from '../utils/APi'
 
 
 export default function VideosLayout({ navigation }) {
-    let ID;
+    let name;
     const [data, setRes] = useState([])
     const [modal, setModal] = React.useState(false);
     const [link, setLink] = useState();
     const fetchData = async () => {
-        ID = await navigation.getParam('id')
-        const response = await get('folder', ID)
-        setRes(response.files)
+        name = await navigation.getParam('name')
+        const response = await get(name)
+        setRes(response)
+        alert(JSON.stringify(data, undefined, 3))
     }
     useEffect(() => {
-        fetchData(data)
-    }, [data])
+        fetchData()
+    }, [name])
 
-    const _modal = data => {
-        setLink(data)
-        setModal(true)
+    // const _modal = data => {
+    //     setLink(data)
+    //     setModal(true)
+    // }
+
+    const _renderList = e => {
+        let str = BASE_URL + e.name;
+        let link = str.split(' ').join('%20')
+        return (
+            <List>
+                <ListItem >
+                    < WebView
+                        style={{ width: 1, minWidth: '160%', height: 210, }}
+                        startInLoadingState={true}
+                        javaScriptEnabled={true}
+                        source={{
+                            html: `<div align="justify"><iframe height=360 width=600 frameBorder='0 'allowFullScreen 
+                            src="${link}"
+                        >
+                        </iframe></div>` }}
+                    />
+                </ListItem>
+
+            </List >
+        )
     }
-
-    const _renderList = e => <List>
-        <ListItem >
-            < WebView
-                style={{ width: 1, minWidth: '160%', height: 210, }}
-                startInLoadingState={true}
-                javaScriptEnabled={true}
-                source={{ html: `<div align="justify"><iframe height=360 width=600 frameBorder='0 'allowFullScreen src="${e.link.slice(0, 22) + '/embed-' + e.link.slice(23)}"></iframe></div>` }}
-            />
-        </ListItem>
-
-    </List >
 
     return (
         <Container>
             <TopHeader drawer="true" text={navigation.getParam('name')} />
-            <Modal onRequestClose={() => setModal(false)} visible={modal}>
+            {/* <Modal onRequestClose={() => setModal(false)} visible={modal}>
                 <Text>hello</Text>
-                   < WebView
-                   style={{display:'flex',flexDirection:'row',flexWrap:'wrap'}}
-                        startInLoadingState={true}
-                        javaScriptEnabled={true}
-                        source={{ html: `<IFRAME SRC="${link}" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=640 HEIGHT=360 allowfullscreen></IFRAME>` }}
-                    />
-            </Modal>
+                < WebView
+                    style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}
+                    startInLoadingState={true}
+                    javaScriptEnabled={true}
+                    source={{ html: `<IFRAME SRC="${link}" FRAMEBORDER=0 MARGINWIDTH=0 MARGINHEIGHT=0 SCROLLING=NO WIDTH=640 HEIGHT=360 allowfullscreen></IFRAME>` }}
+                />
+            </Modal> */}
             <FlatList
                 data={data}
                 renderItem={({ item }) => _renderList(item)}
-                keyExtractor={item => item.link}
+                key={item => item.name}
             />
         </Container>
     )

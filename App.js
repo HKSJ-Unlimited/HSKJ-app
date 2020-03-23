29f11711d083065cfa75c49242f135bcb4bedd8d
rocknegi <rocknegi53@gmail.com>
2020-03-23 21:27:02 +0530

    Add Theme Toogle use Context API save the Toogle Choice to AsyncStorage

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
-import { mapping, dark as darkTheme } from '@eva-design/eva';
+import { ApplicationProvider} from '@ui-kitten/components';
+import { mapping, light,dark } from '@eva-design/eva';
 
-import { default as appTheme } from './custom-theme.json';
 import settings from './containers/settings';
 import Categories from './containers/Categories';
 import VideosLayout from './components/VideosLayout';
 import SelectedCategory from './containers/SelectedCategory';
 import Home from './containers/Home';
+import { ThemeContext } from './theme-context';
 
 const AppNavigator = createStackNavigator(
   {
@@ -31,13 +31,22 @@ const AppSwitchNavigator = createAppContainer(
   }),
 );
 
-const theme = { ...darkTheme, ...appTheme };
-
 const App = () => {
-  return (
-    <ApplicationProvider mapping={mapping} theme={theme}>
-      <AppSwitchNavigator />
+const themes = { light,dark };
+const [theme, setTheme] = React.useState('light');
+const currentTheme = themes[theme];
+
+const toggleTheme = () => {
+  const nextTheme = theme === 'light' ? 'dark' : 'light';
+  setTheme(nextTheme);
+ 
+};
+ return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ApplicationProvider mapping={mapping} theme={currentTheme}>
+      <AppSwitchNavigator/>
     </ApplicationProvider>
+  </ThemeContext.Provider>
   );
 };
 export default App

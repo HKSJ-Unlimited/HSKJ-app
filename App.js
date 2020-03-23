66c20cbe83deaf64dfd297dbd3f1fcce18cd88b8
rocknegi <rocknegi53@gmail.com>
2020-03-23 15:19:04 +0530

    integerate React Native Kitten UI

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
+import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+import { mapping, dark as darkTheme } from '@eva-design/eva';
 
+import { default as appTheme } from './custom-theme.json';
 import settings from './containers/settings';
 import Categories from './containers/Categories';
 import VideosLayout from './components/VideosLayout';
@@ -9,7 +13,7 @@ import Home from './containers/Home';
 
 const AppNavigator = createStackNavigator(
   {
-    home:{screen:Home},
+    home: {screen: Home},
     categories: {screen: Categories},
     selectedCategory: {screen: SelectedCategory},
     videosLayout: {screen: VideosLayout},
@@ -21,8 +25,19 @@ const AppNavigator = createStackNavigator(
   },
 );
 
-const AppSwitchNavigator = createSwitchNavigator({
-  dashboard: {screen: AppNavigator},
-});
+const AppSwitchNavigator = createAppContainer(
+  createSwitchNavigator({
+    dashboard: {screen: AppNavigator},
+  }),
+);
 
-export default createAppContainer(AppSwitchNavigator);
+const theme = { ...darkTheme, ...appTheme };
+
+const App = () => {
+  return (
+    <ApplicationProvider mapping={mapping} theme={theme}>
+      <AppSwitchNavigator />
+    </ApplicationProvider>
+  );
+};
+export default App

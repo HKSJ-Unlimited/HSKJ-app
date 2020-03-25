4c96d5973be5f86a759582f6febf3353a63f6724
rocknegi <rocknegi53@gmail.com>
2020-03-25 15:59:59 +0530

    add loader and other stuff

--- containers/SelectedCategory.js
+++ containers/SelectedCategory.js
@@ -1,87 +1,103 @@
 import React, {useState, useEffect} from 'react';
-import { FlatList,BackHandler} from 'react-native';
+import {FlatList, BackHandler} from 'react-native';
 import Layout from '../components/Layout';
 import {get} from '../utils/APi';
 // import { List, ListItem } from 'native-base';
-import { InterstitialAd, TestIds } from '@react-native-firebase/admob';
-import { AdEventType } from '@react-native-firebase/admob';
-import {Text,List,ListItem,Layout as View} from '@ui-kitten/components';
+import {InterstitialAd, TestIds} from '@react-native-firebase/admob';
+import {AdEventType} from '@react-native-firebase/admob';
+import {Text, List, ListItem, Layout as View} from '@ui-kitten/components';
 
 import {ThemeContext} from '../theme-context';
-import { INTERSTITIAL } from '../ADS/AD-IDs';
+import {INTERSTITIAL} from '../ADS/AD-IDs';
 import TopHeader from '../components/Header';
+import Loader from './Loader';
 
 const interstitial = InterstitialAd.createForAdRequest(INTERSTITIAL, {
-    requestNonPersonalizedAdsOnly: true,
+  requestNonPersonalizedAdsOnly: true,
 });
 
-interstitial.onAdEvent((type) => {
-    if (type === AdEventType.LOADED) {
-      interstitial.show();
-    }
-  });  
+interstitial.onAdEvent(type => {
+  if (type === AdEventType.LOADED) {
+    interstitial.show();
+  }
+});
 
 const SelectedCategory = ({navigation}) => {
   const themeContext = React.useContext(ThemeContext);
   const [data, setRes] = useState([]);
-  const [theme,setTheme] = useState('')
-if(navigation.state.routeName==='selectedCategory'){
-  // interstitial.load();
-}
+  const [theme, setTheme] = useState('');
+  if (navigation.state.routeName === 'selectedCategory') {
+    interstitial.load();
+  }
   useEffect(() => {
-    setTheme(themeContext.theme)
+    setTheme(themeContext.theme);
     fetchData();
-    return(()=>setRes(0))
-  },[navigation]);
+    return () => setRes(0);
+  }, [navigation]);
 
   const fetchData = async () => {
-    const response = await get(navigation.getParam('name')+'/');
+    const response = await get(navigation.getParam('name') + '/');
     setRes(response);
   };
 
   const _renderList = item => {
-      const regex = /on SexyPorn|.mp4|Pornhub.com|YesPornPlease|[0-9]/gi;
-      let name = item.name.replace(regex,"").slice(0,120)
-      return(
-        <ListItem
-        style={{height:90,marginTop:5,borderRadius:6}}
-          onPress={() =>
-            navigation.navigate('videosLayout', {name: navigation.getParam('name')+'/'+item.name})
-          }>
-          <Text style={{fontSize:15,textAlign:'center',flex:1,margin:5,fontFamily:'Lato-Regular',}}>{name}</Text>
-        </ListItem>
-      )
-  }
+    const regex = /on SexyPorn|.mp4|Pornhub.com|YesPornPlease|[0-9]/gi;
+    let name = item.name.replace(regex, '').slice(0, 120);
+    return (
+      <ListItem
+        style={{height: 90, marginTop: 5, borderRadius: 6}}
+        onPress={() =>
+          navigation.navigate('videosLayout', {
+            name: navigation.getParam('name') + '/' + item.name,
+          })
+        }>
+        <Text
+          style={{
+            fontSize: 15,
+            textAlign: 'center',
+            flex: 1,
+            margin: 5,
+            fontFamily: 'Lato-Regular',
+          }}>
+          {name}
+        </Text>
+      </ListItem>
+    );
+  };
 
-  const _renderSeperator = () => 
-  <View
-  style={
-    theme==='light'?
-    {
-    height: 4,
-    // marginTop:2,
-    // backgroundColor: "#121212",
-  }
-  :{
-    height: 2,
-    marginTop:2,
-    backgroundColor: "#F0B823",
-  }
-  }
-/>
+  const _renderSeperator = () => (
+    <View
+      style={
+        theme === 'light'
+          ? {
+              height: 4,
+              // marginTop:2,
+              // backgroundColor: "#121212",
+            }
+          : {
+              height: 2,
+              marginTop: 2,
+              backgroundColor: '#F0B823',
+            }
+      }
+    />
+  );
   return (
-    <View style={{flex:1}}>
-      <TopHeader text={navigation.getParam('name').slice(0,28)}/>
-     <List
-     style={{backgroundColor:theme==='light'?'#F2F6FF':'#000'}}
-        contentContainerStyle={{paddingBottom: 100,marginHorizontal:10,}}
-        data={data}
-        renderItem={({item}) => _renderList(item)}
-        key={item => item.id}
-        ItemSeparatorComponent={_renderSeperator}
-      />
+    <View style={{flex: 1}}>
+      <TopHeader text={navigation.getParam('name').slice(0, 28)} />
+      {data.length>1 ? (
+        <List
+          style={{backgroundColor: theme === 'light' ? '#F2F6FF' : '#000'}}
+          contentContainerStyle={{paddingBottom: 100, marginHorizontal: 10}}
+          data={data}
+          renderItem={({item}) => _renderList(item)}
+          key={item => item.id}
+          ItemSeparatorComponent={_renderSeperator}
+        />
+      ) : (
+        <Loader />
+      )}
     </View>
-
   );
 };
 

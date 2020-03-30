2f2aa2338e16700ab6a1be656dc9194ba121030b
rocknegi <rocknegi53@gmail.com>
2020-03-30 23:01:45 +0530

    change Thumbnail rendering logic

--- utils/Data.js
+++ utils/Data.js
@@ -4,60 +4,70 @@ export const HotPicks = [
       id:'Czech',
       uri:
         'https://static.v2.paysites.czechcash.com/media/czechav.com/images/opengraph.jpg?190912',
+        folder:'1V28XMsQLgYbGOyWXkaZqT2oqA46AUmeq'
     },
     {
       name: 'Public Agent',
       id:'Public-Agent',
       uri:
         'https://img-hw.xnxx-cdn.com/videos/thumbslll/84/33/79/843379010e4de1afdb85707dcc1c7cb7/843379010e4de1afdb85707dcc1c7cb7.2.jpg',
+        folder:'1SfMZ9suE2rmzMUxY0kQ5d7qZfLlcDKNK'
     },
     {
       name: 'Brazzers',
       id:'ZZ',
       uri:
         'https://www.logolynx.com/images/logolynx/3e/3e0f748f9c306916012933bea5c7cd45.jpeg',
+        folder:'1r9oo1Zm06048nwfzzh-GjnZdjFkr3YDi'
     },
     {
       name: 'Family Therapy',
       id:'Family Therapy',
       uri:
         'https://img-hw.xvideos-cdn.com/videos/thumbslll/99/d4/96/99d496c620731ff8bbf22d637b566ddd/99d496c620731ff8bbf22d637b566ddd.4.jpg',
+        folder:'1RrUAe1spWXbUPtK10-OR2KKwliJHuDfg'
     },
     {
       name: 'Sis Loves Me',
       id:'Sis Loves Me',
       uri:
         'https://www.pornsiteoffers.com/wp-content/uploads/2017/02/sis-loves-me-900x450.jpg',
+        folder:'1cLtqnIXkgMUebI-fouCckHIMkGsBXioH'
     },
     {
       name: 'Bratty Sis',
       id:'Bratty Sis',
       uri:
         'https://brattyfamily.com/wp-content/uploads/2019/07/i_bet_youre_wet-480x270.jpg',
+        folder:'1RV1om1-BJLbK9E_VYLRfRjiBlLy5yabH'
     },
     {
       name: 'Naughty America',
       id:'America',
       uri:
         'https://www.dailydot.com/wp-content/uploads/89c/f6/42fbd26dd0db6a9b.png',
+        folder:'1Q9FPobHROFWHn29mngjAaAV9ia1XXzda'
     },
     {
       name: 'Vixen',
       id:'Vixen',
       uri:
         'https://www.pornsiteoffers.com/wp-content/uploads/2016/10/vixen.com_-1024x450.jpg',
+        folder:'1hKMNhBzJ5JSmRWCGu9RTsc6SSaZAw4S6'
     },
     {
       name: 'Fake Taxi',
       id:'TAXI',
       uri:
         'https://cdn77-pic.xvideos-cdn.com/videos/thumbslll/af/42/8d/af428d45ccfa18d2ac7a38d954164a5b/af428d45ccfa18d2ac7a38d954164a5b.12.jpg',
+        folder:'1F0GBZmD0uI8I3N6Y6j4l484-T78bC3aH'
     },
     {
       name: 'Bang Bus',
       id:'BUS',
       uri:
         'https://xxxstyanulo.net/uploads/posts/2019-11/1574898832_xxxstyanulo_net-bang-bus-with-roxy-ryder-in-sd-quality-by-bangbus.jpg',
+        folder:'15aSWQUnqYumJunrBO1UO_Pv4jl8wOpxh'
     },
   ];
  
@@ -65,238 +75,285 @@ export const HotPicks = [
       {
         name:'Amateur',
         id:'Amateur',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924980-f1d8e700-72c1-11ea-940c-c1ba51d69615.jpg',
+        folder:'1dMM5AV9bCZCafVByXkBTuw_RV5rQ7fob'
       },
       {
         name:'Naughty America',
         id:'America',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924981-f1d8e700-72c1-11ea-9375-9502efbb4b35.jpg',
+        folder:'1Q9FPobHROFWHn29mngjAaAV9ia1XXzda'
       },
       {
         name:'NubilePorn/21st Century Porn/PassionHD',
         id:'Art-wow-21st-nubile-Passion-similar',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924982-f2717d80-72c1-11ea-8784-ba5e83b418f9.jpg',
+        folder:'15ByO5sFQs8_bwBuwgnFFUlUui5uYtgIk'
       },
       {
         name:'Gang Bang',
         id:'Bang Bang',
-        uri:''
+        uri:'https://github-production-user-asset-6210df.s3.amazonaws.com/10401638/77924985-f30a1400-72c1-11ea-9b19-94da86e16848.jpg',
+        folder:'1pzlyde0WYpu0xcxU0Q7aMEWUCVVIwZcD'
       },
       {
         name:'BBC',
         id:'BBC',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924988-f3a2aa80-72c1-11ea-982d-6ccd449676d6.jpg',
+        folder:'1w5qBNSBDqm76HHBiUmi-Q8jsnNiAKtEc'
       },
       {
         name:'Bratty Sis',
         id:'Bratty Sis',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924992-f4d3d780-72c1-11ea-871a-09bbcc384f24.jpg',
+        folder:'1RV1om1-BJLbK9E_VYLRfRjiBlLy5yabH'
       },
       {
         name:'Brazzers',
         id:'ZZ',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924978-f1405080-72c1-11ea-8aff-99328958e4d6.jpg',
+        folder:'1r9oo1Zm06048nwfzzh-GjnZdjFkr3YDi'
       },
       {
         name:'Bang Bus',
         id:'Bus',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924996-f56c6e00-72c1-11ea-9cca-58a7a5f3fef2.jpg',
+        folder:'15aSWQUnqYumJunrBO1UO_Pv4jl8wOpxh'
       },
       {
         name:'Casting',
         id:'Casting',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924875-df5ead80-72c1-11ea-9583-847c944c25aa.jpg',
+        folder:'18OOIFXaotQ_BDvGPuv43szvTyq46cdJ1'
       },
       {
         name:'Czech',
         id:'Czech',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924881-e1287100-72c1-11ea-9bbb-b953a36dc1f8.jpg',
+        folder:'1V28XMsQLgYbGOyWXkaZqT2oqA46AUmeq'
       },
       {
         name:'Step Siblings/Parents',
         id:'DAD_SIS_MOM_BRO',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924883-e1c10780-72c1-11ea-8db4-f2cab77007f6.jpg',
+        folder:'1VGH-3u_GFSxc7RJM5LiriwMr0nbVFWd6'
       },
       {
         name:'Dani Daniels',
         id:'Dani Daniels',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924884-e1c10780-72c1-11ea-9788-3f4e0aef8ac3.jpg',
+        folder:'1i7n21iAkBP6Rrg4l3hqeuZSALAoFFm3A'
       },
       {
         name:'Defloration',
         id:'Defloration',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924887-e2599e00-72c1-11ea-8421-f3a9b1fe56c1.jpg',
+        folder:'1gAJ7oaav3NK7Cjq7cHSp9tCtu9kaC2RS'
       },
       {
         name:'Digital Playground',
         id:'Digital Playground',
-        uri:''
+        uri:'http://user-images.githubusercontent.com/10401638/77924888-e2f23480-72c1-11ea-93eb-ea83d133812e.jpg',
+        folder:'1EhvrK6svM0jBu5dG3UKC979ODDxGvM7S'
       },
       {
         name:'Family Therapy',
         id:'Family Therapy',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924893-e38acb00-72c1-11ea-8206-20e6d2fb57bf.jpg',
+        folder:'1RrUAe1spWXbUPtK10-OR2KKwliJHuDfg'
       },
       {
         name:'Porn Fidelity',
         id:'Fidelity',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924895-e4236180-72c1-11ea-8960-d988033b49bc.jpg',
+        folder:'1zjoqSh0t4US-m6v-YrP8UoffdPxvyJow'
       },
       {
         name:"My Friend's XYZ",
         id:"Friend's xyz",
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924896-e4236180-72c1-11ea-94e0-36aabf849981.jpg',
+        folder:'1yBiqhVXEN4aJcQYzi9xoq_79YXNGm4JH'
       },
       {
         name:'Girls Do Porn',
         id:'Gdpo',
-        uri:''
+        uri:'http://user-images.githubusercontent.com/10401638/77924898-e4bbf800-72c1-11ea-8cbe-246f85eb1f6d.jpg',
+        folder:'1U1hj4bZOm7bXM8qDNnuTQ6fGZQnyfjNT'
       },
       {
         name:'Girlfriend Experience',
         id:'GF',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924901-e5548e80-72c1-11ea-882f-2ef19872d07f.jpg',
+        folder:'1r-AGotCfeQebEZmqcTu39vdswnLSkV4n'
       },
       {
         name:'Indian',
         id:'INDIAN',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924902-e5548e80-72c1-11ea-872e-6dbc468bcb62.jpg',
+        folder:'1yOGV1sSNDx-PiCHxaHJm7i0SEh2VBekn'
       },
       {
         name:'Innocent High',
         id:'Innocent High',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924904-e5ed2500-72c1-11ea-955f-048a603e0aea.jpg',
+        folder:'1JCPj19KHq5VzEWPa8V0VORXJMwa3erVB'
       },
 
       {
         name:'Japanese Censored',
         id:'japanese censored',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924907-e685bb80-72c1-11ea-8f0c-8187ddb7f501.jpg',
+        folder:'1YN57EGE35U3uqvuQEPw66HQkgjHvPFWY'
       },
       {
         name:'japanese uncensored',
         id:'japaneseUncensored',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924911-e685bb80-72c1-11ea-8732-55f54c53e6e1.jpg',
+        folder:'1YN57EGE35U3uqvuQEPw66HQkgjHvPFWY'
       },
       {
         name:'Legal Porno',
         id:'Legal',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924915-e71e5200-72c1-11ea-899c-f2b3cd18e100.jpg',
+        folder:'1BJxsog9WH690ibZ7hbqW3MpU8WB49rBo'
       },
       {
         name:'Nuru Massage',
         id:'Massage',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924917-e7b6e880-72c1-11ea-88ed-f06220196d72.jpg',
+        folder:'1dZzPqHViEIFiMlDYUyQPw6n1O2wBSh0Z'
       },
       {
         name:'Mia Khalifa',
         id:'Mia Khalifa',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924919-e7b6e880-72c1-11ea-98e6-122318f27b9d.jpg',
+        folder:'1UpdPj82A8RqoBB_-_J9wPq5u8XW9hkGY'
       },
       {
         name:'MIX Stuff',
         id:'MIX',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924923-e84f7f00-72c1-11ea-80de-63b9c9914b8f.jpg',
+        folder:'1cdbK3pdjR2Bd1wp-BZmsKWSik8eHS9uE'
       },
       {
         name:'MOFOS',
         id:'MOFOS',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924924-e8e81580-72c1-11ea-8f63-f1dccc4c11a4.jpg',
+        folder:'1nBBaZfAm9HNJYkYbIEX7NR1YvgucRCSf'
       },
       {
         name:'Movies like',
         id:'Movies',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924926-e8e81580-72c1-11ea-801d-cf3e6ca2a02c.jpg',
+        folder:'1WpdqNgGWNjR4Hhbe5eaeE1-FWVHGUvvX'
       },
       {
         name:'POV and Lesbian',
         id:'POV -lesbo',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924930-e980ac00-72c1-11ea-8454-b53eb9a89a2d.jpg',
+        folder:'1QK14GbMoJpb6OMKmtjQZAlNy6xP7krN5'
       },
       {
         name:'Property Sex',
         id:'Property',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924934-ea194280-72c1-11ea-9107-1bd1cae5cf1a.jpg',
+        folder:'14Nh6z7X3gY5VMXopRH8xsqHtQk_SaVDi'
       },
       {
         name:'Public Agent',
         id:'Public-Agent',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924936-eab1d900-72c1-11ea-80b4-3503c7b30f9d.jpg',
+        folder:'1SfMZ9suE2rmzMUxY0kQ5d7qZfLlcDKNK'
       },
       {
         name:'Reality King/Prime',
         id:'Rk Prime',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924943-eb4a6f80-72c1-11ea-8645-8ada47249b4a.jpg',
+        folder:'1TSQQ8kBubODN9xvcpo_RyYkj2A1PLO_m'
       },
       {
         name:'Russian Institute',
         id:'RussiaInstitute',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924948-ebe30600-72c1-11ea-89f7-b240c61fb4f9.jpg',
+        folder:'17Mp1e0M_7RIvmnBzxEzhwT4usaLRQVUS'
       },
       {
         name:'Russian Amateur',
         id:'RussianAmaetur',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924948-ebe30600-72c1-11ea-89f7-b240c61fb4f9.jpg',
+        folder:'18pJFWIrJoR2m7PW5EOZfyR-fOD5erJRk'
       },
       {
         name:'Savita Bhabhi',
         id:'Savita Bhabhi',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924949-ec7b9c80-72c1-11ea-9b8f-c387d5d8d48f.jpg',
+        folder:'1jPLQ44YCUsYBVvB0nSNYA5HIHE5tLqkO'
       },
       {
         name:'Shop Lyfter',
         id:'ShopLyfter',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924952-ed143300-72c1-11ea-8e7d-859ddc64b180.jpg',
+        folder:'1iZKWrzFJO7nQ7rOEE5-F4GeTCuh5aRdy'
       },
       {
         name:'Sweet Sinners',
         id:'Sinners',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924954-ed143300-72c1-11ea-830b-bb78ce3c4615.jpg',
+        folder:'1cIPNY_pAkK51ZPBTDyMM2_Yr1OKHjqRQ'
       },
       {
         name:'Sis Loves Me',
         id:'Sis Loves Me',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924955-edacc980-72c1-11ea-9c2c-1701f65fbfc8.jpg',
+        folder:'1cLtqnIXkgMUebI-fouCckHIMkGsBXioH'
       },
       {
         name:'Taboo',
         id:'Taboo',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924958-ee456000-72c1-11ea-8973-241754f90a7c.jpg',
+        folder:'1DqQ0yKp_nha2JHCcYwq5QiPphp_Gjulg'
       },
       {
         name:'Fake Taxi',
         id:'TAXI',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924890-e2f23480-72c1-11ea-9a1f-7c8f29ec38cb.jpg',
+        folder:'1F0GBZmD0uI8I3N6Y6j4l484-T78bC3aH'
       },
       {
         name:'Teacher',
         id:'Teacher',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924961-ee456000-72c1-11ea-98c3-c530d34eab94.jpg',
+        folder:'1A23H2uYjsBBbuZIDIy6YPdr0KbccTkQc'
       },
       {
         name:'TeamSkeet',
         id:'TeamSkeet',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924963-eeddf680-72c1-11ea-8461-320a530536fe.jpg',
+        folder:'1m-O_4SAWx6MVHQ5_AMHHsvth9FUANQDt'
       },
       {
         name:'TRIAL',
         id:'TRIAL',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924968-ef768d00-72c1-11ea-814f-e8272a3571a8.jpg',
+        folder:'1CnBwMrgu_r67pNN4aeYYNPiqnuLX4_SR'
       },
       {
         name:'Tushy',
         id:'Tushy',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924969-ef768d00-72c1-11ea-85db-23f5bc89a5d6.jpg',
+        folder:'1XgOiwgZvo85lqTg5ERpkBnl8dEJkCwBt'
       },
       {
         name:'Ultra Films',
         id:'UltraFilms',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924971-f00f2380-72c1-11ea-9cce-e83366a4a28f.jpg',
+        folder:'10LB1NTE2zNjddgI_KutbFTAyZS6yYOts'
       },
       {
         name:'Vixen',
         id:'Vixen',
-        uri:''
+        uri:'https://user-images.githubusercontent.com/10401638/77924973-f0a7ba00-72c1-11ea-93b9-26ee9cc5561b.jpg',
+        folder:'1hKMNhBzJ5JSmRWCGu9RTsc6SSaZAw4S6'
       },
 
 

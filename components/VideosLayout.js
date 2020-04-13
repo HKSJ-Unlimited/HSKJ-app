da0c6897bbf777ededc3f32ca1dc7cae8163b19b
rocknegi <rocknegi53@gmail.com>
2020-04-13 16:14:23 +0530

    Create video controls from scratch

--- components/VideosLayout.js
+++ components/VideosLayout.js
@@ -1,160 +1,186 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {
   Dimensions,
   StyleSheet,
   PermissionsAndroid,
   StatusBar,
-  BackHandler
+  BackHandler,
+  TouchableWithoutFeedback,
+  TouchableOpacity
 } from 'react-native';
-import {Button} from 'native-base';
+import {Button, Fab} from 'native-base';
 import {BASE_URL} from 'react-native-dotenv';
 import {BannerAdSize} from '@react-native-firebase/admob';
 import RNFetchBlob from 'rn-fetch-blob';
 import {Text, Layout as View} from '@ui-kitten/components';
-import VideoPlayer from 'react-native-video-controls';
 import Orientation from 'react-native-orientation';
+import Video from 'react-native-video';
 
-import {ThemeContext} from '../theme-context';
+import {FullscreenClose, FullscreenOpen} from '../assets/icons';
 import TopHeader from './Header';
 import GoogleADBanner from '../ADS/GoogleADBanner';
+import { Icon } from 'native-base';
+import PlayerControls from './PlayerControls';
 
-const {width: screenWidth} = Dimensions.get('window');
-
-export default function VideosLayout({navigation}) {
-  const name = BASE_URL + navigation.getParam('name');
-  const {width} = Dimensions.get('window');
-  const height = width * 0.5625;
-  const [fullscreen, setFullscreen] = useState(false);
-  const themeContext = React.useContext(ThemeContext);
-  const videoRef = React.createRef();
-  const [state,setState] = useState({
-    currentTime:0,
-    duration:0
-  })
-
-  const _handleOrientation = orientation => {
-    orientation === 'LANDSCAPE' ? setFullscreen(true) : setFullscreen(false);
+export default class VideosLayout extends React.Component {
+  state = {
+    fullscreen: false,
+    duration: 0,
+    currentTime: 0,
+    isLoading: true,
+    paused: false,
+    play:false,
+    nane: '',
+    showControls:false
   };
-  useEffect(() => {
-    BackHandler.addEventListener("hardwareBackPress",_backHandler)
-    Orientation.addOrientationListener(_handleOrientation);
-    return () => {
-      Orientation.removeOrientationListener(_handleOrientation);
-      BackHandler.removeEventListener("hardwareBackPress",_backHandler)
-    };
-  }, []);
-  const _backHandler = ()=>{
+  _onLoadHandler = data => {
+    this.setState(s => ({
+      ...s,
+      duration: data.duration,
+    }));
+  };
+
+  _HandleProgress = data => {
+    this.setState(s => ({
+      ...s,
+      currentTime: data.currentTime,
+    }));
+  };
+
+  _handleOrientation = orientation => {
+    orientation === 'LANDSCAPE'
+      ? this.setState({fullscreen: true})
+      : this.setState({fullscreen: false});
+  };
+
+  _HandleFullscreen = () => {
     Orientation.unlockAllOrientations();
-    Orientation.lockToPortrait()
-  }
-  const _HandleFullscreen = () => {
-    Orientation.unlockAllOrientations();
-    fullscreen
+    this.state.fullscreen
       ? Orientation.lockToPortrait()
       : Orientation.lockToLandscapeLeft();
   };
-  async function requestStoragePermission() {
-    try {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
-        {
-          title: 'HKSJ needs Storage Permission',
-          message:
-            'HKSJ needs access to your Device Storage uwu ' +
-            'so you can fap when offline.',
-          buttonNeutral: 'Ask Me Later',
-          buttonNegative: 'Cancel',
-          buttonPositive: 'OK',
-        },
-      );
-      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        let dirs = RNFetchBlob.fs.dirs.DownloadDir + '/HKSJ';
-        const android = RNFetchBlob.android;
-        const fileName = name.replace(BASE_URL, '');
-        RNFetchBlob.config({
-          addAndroidDownloads: {
-            useDownloadManager: true,
-            notification: true,
-            description: 'File downloaded.',
-            path: dirs + `/${fileName}`,
-            mediaScannable: true,
-            title: fileName,
-          },
-        })
-          .fetch('GET', name)
-          .then(res => {
-            android.actionViewIntent(res.path(), 'image/png');
-          })
-          .catch(err => {
-            alert('Deer you canceled the download');
-          });
-      } else {
-        Alert('Cum on Nibba you want to download or not?');
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  const download = () => {
-    requestStoragePermission();
+  _backHandler = () => {
+    Orientation.unlockAllOrientations();
+    Orientation.lockToPortrait();
   };
-const _onLoadHandler = (data)=>{
-  setState(s => ({
-    ...s,
-    duration: data.duration,
-    currentTime: data.currentTime,
-  }));
-}
-const _HandleProgress = (data)=>{
-  setState(s => ({
-    ...s,
-    currentTime: data.currentTime,
-  }));
-}
+  componentDidMount() {
+    const name = BASE_URL + this.props.navigation.getParam('name');
+    this.setState({name});
+    BackHandler.addEventListener('hardwareBackPress', this._backHandler);
+    Orientation.addOrientationListener(this._handleOrientation);
+  }
+  componentWillUnmount() {
+    Orientation.removeOrientationListener(this._handleOrientation);
+    BackHandler.removeEventListener('hardwareBackPress', this._backHandler);
+  }
 
-  const VideoView = () => (
-    <VideoPlayer
-      controls={false}
-      // onLoad={_onLoadHandler}
-      // ref={videoRef}
-      style={fullscreen ? styles.fullscreenVideo : styles.video}
-      source={{uri: name}}
-      paused={true}
-      onEnterFullscreen={_HandleFullscreen}
-      onExitFullscreen={_HandleFullscreen}
-      // onProgress={_HandleProgress}
-      disableBack
-      disableVolume
-      // onEnd={()=>videoRef.current.seek(0)}
-    />
-  );
-
-  return (
-    <View style={{flex: 1}}>
-      <StatusBar hidden={fullscreen}/>
-      {!fullscreen && <TopHeader text="HKSJ" />}
-      <VideoView />
-      {!fullscreen && (
-        <>
-          {/* <View style={(styles.banner, { marginLeft:'5%',marginTop: 5})}>
-            <GoogleADBanner type={BannerAdSize.BANNER} name="VIDEO_TOP" />
-          </View> */}
-          <Button full style={styles.button} onPress={() => download()}>
-            <Text style={{color: '#eee'}}>Download</Text>
-          </Button>
-          <View style={styles.banner}>
-            <GoogleADBanner
-              type={BannerAdSize.MEDIUM_RECTANGLE}
-              name="VIDEO_BOTTOM"
-            />
-              <GoogleADBanner type={BannerAdSize.BANNER} name="VIDEO_TOP" />
-          </View>
-        </>
-      )}
+  renderToolbar = () => (
+    <View>
+      <Text> {this.props.navigation.getParam('name')} </Text>
     </View>
   );
-}
 
+  onPaused = playerState => {
+    this.setState({
+      paused: !this.state.paused,
+      playerState,
+    });
+  };
+
+  onSeek = seek => {
+    this.videoPlayer.seek(seek);
+  };
+
+  onSeeking = currentTime => this.setState({currentTime});
+
+  _showControls = () => {
+    this.state.showControls
+    ? this.setState({showControls: false})
+    : this.setState({showControls: true});
+  };
+
+  handlePlayPause=()=> {
+    if (this.state.play) {
+      this.setState({play: false, showControls: true});
+      return;
+    }
+    this.setState({play: true});
+    setTimeout(() => this.setState(s => ({...s, showControls: false})), 2000);
+  }
+   skipBackward=()=>  {
+    this.player.seek(this.state.currentTime - 15);
+    this.setState({currentTime: this.state.currentTime - 15});
+  }
+
+  skipForward=()=>  {
+    this.player.seek(this.state.currentTime + 15);
+    this.setState({currentTime: this.state.currentTime + 15});
+  }
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <StatusBar hidden={this.state.fullscreen} />
+        {!this.state.fullscreen && <TopHeader text="HKSJ" />}
+        <TouchableWithoutFeedback onPress={this._showControls}>
+          <View style={{flex: 1}}>
+            <Video
+              controls={false}
+              ref={(ref) => {
+                this.player = ref
+              }} 
+              onLoad={this._onLoadHandler}
+              style={
+                this.state.fullscreen ? styles.fullscreenVideo : styles.video
+              }
+              source={{
+                uri:
+                  'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+              }}
+              onEnterFullscreen={this._HandleFullscreen}
+              onExitFullscreen={this._HandleFullscreen}
+              onProgress={this._HandleProgress}
+              paused={this.state.play}
+            />
+            {this.state.showControls && (
+              <View style={styles.controlOverlay}> 
+               <TouchableOpacity
+                  onPress={this._HandleFullscreen}
+                  hitSlop={{top: 10, bottom: 10, left: 10, right: 10}}
+                  style={styles.fullscreenButton}>
+                  {this.state.fullscreen ? <FullscreenClose /> : <FullscreenOpen />}
+                </TouchableOpacity> 
+                 <PlayerControls
+                  onPlay={this.handlePlayPause}
+                  onPause={this.handlePlayPause}
+                  playing={this.state.play}
+                  showPreviousAndNext={false}
+                  showSkip={true}
+                  skipBackwards={this.skipBackward}
+                  skipForwards={this.skipForward}
+                /> 
+             </View> 
+            )}
+          </View> 
+         </TouchableWithoutFeedback> 
+
+        {!this.state.fullscreen && (
+          <>
+            <Button full style={styles.button} onPress={() => download()}>
+              <Text style={{color: '#eee'}}>Download</Text>
+            </Button>
+            <View style={styles.banner}>
+              <GoogleADBanner
+                type={BannerAdSize.MEDIUM_RECTANGLE}
+                name="VIDEO_BOTTOM"
+              />
+              <GoogleADBanner type={BannerAdSize.BANNER} name="VIDEO_TOP" />
+            </View>
+          </>
+        )}
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
   Video: {
     width: Dimensions.get('window').width * 1.6,
@@ -162,28 +188,44 @@ const styles = StyleSheet.create({
   },
   banner: {
     flex: 1,
-    marginLeft:'10%',
-    flexDirection:'column',
-    justifyContent:'space-between'
+    marginLeft: '10%',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
   },
   button: {
     marginHorizontal: '10%',
     marginBottom: 5,
-    marginTop:5,
+    marginTop: 5,
     backgroundColor: '#C2913F',
   },
   video: {
-    flex:1,
+    flex: 1,
     height: Dimensions.get('window').width * (9 / 16),
     width: Dimensions.get('window').width,
     backgroundColor: 'black',
-    marginTop:10,
+    marginTop: 10,
     // backgroundColor:'black'
   },
   fullscreenVideo: {
-    flex:1,
+    flex: 1,
     height: Dimensions.get('window').width,
     width: Dimensions.get('screen').height,
     backgroundColor: 'black',
+  },
+  fullscreenButton: {
+    flex: 1,
+    flexDirection: 'row',
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    paddingRight: 10,
+  },
+  controlOverlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#000000c4',
+    justifyContent: 'space-between',
   },
 });

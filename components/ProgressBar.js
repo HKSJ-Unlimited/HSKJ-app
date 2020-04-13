8723ac3a051a6443f2c62e092e279d27f601f34d
rocknegi <rocknegi53@gmail.com>
2020-04-13 17:38:49 +0530

    add progress bar

---
+++ components/ProgressBar.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import Slider from '@react-native-community/slider';
+import {View, Text, StyleSheet} from 'react-native';
+
+const getMinutesFromSeconds = time => {
+  const minutes = time >= 60 ? Math.floor(time / 60) : 0;
+  const seconds = Math.floor(time - minutes * 60);
+
+  return `${minutes >= 10 ? minutes : '0' + minutes}:${
+    seconds >= 10 ? seconds : '0' + seconds
+  }`;
+};
+
+const ProgressBar = ({
+  currentTime,
+  duration,
+  onSlideCapture,
+  onSlideStart,
+  onSlideComplete,
+}) => {
+  const position = getMinutesFromSeconds(currentTime);
+  const fullDuration = getMinutesFromSeconds(duration);
+  const handleOnSlide = time => {
+    onSlideCapture({seekTime: time});
+  };
+  return (
+    <View style={styles.wrapper}>
+      <Slider
+        value={currentTime}
+        minimumValue={0}
+        maximumValue={duration}
+        step={1}
+        onValueChange={handleOnSlide}
+        onSlidingStart={onSlideStart}
+        onSlidingComplete={onSlideComplete}
+        minimumTrackTintColor={'#F44336'}
+        maximumTrackTintColor={'#FFFFFF'}
+        thumbTintColor={'#F44336'}
+        style={{height: 50}}
+      />
+      <View style={styles.timeWrapper}>
+        <Text style={styles.timeLeft}>{position}</Text>
+        <Text style={styles.timeRight}>{fullDuration}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ProgressBar;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  timeWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 5,
+  },
+  timeLeft: {
+    flex: 1,
+    fontSize: 16,
+    color: '#FFFFFF',
+    paddingLeft: 10,
+  },
+  timeRight: {
+    flex: 1,
+    fontSize: 16,
+    color: '#FFFFFF',
+    textAlign: 'right',
+    paddingRight: 10,
+  },
+});

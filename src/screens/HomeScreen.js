16c627741a0b7750c1dd093147592662266e014c
rohit singh negi <rocknegi53@gmail.com>
2020-07-13 15:21:51 +0530

    Scaffolding react native navigation v5

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, SafeAreaView} from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
